419362538d274a179006d0ebec67692806a52647
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-17 13:22:49 -0600

    hazard warning drill

--- higherorder.js
+++ higherorder.js
@@ -41,3 +41,33 @@ const filteredNames = filter(myNames, function(name) {
 
 console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter = 0;
+  return function(location){
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${(1 === warningCounter) ? 'time' : 'times'} today!`)
+  }
+
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const avalancheWarning = hazardWarningCreator('avalanche closure');
+const wildfireWarning = hazardWarningCreator('wildfire')
+
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+// Invoke the other hazard creators you instantiated, too
+
+avalancheWarning('I-70 between Frisco and Copper Mountain');
+avalancheWarning('the Eisenhower-Johnson Tunnel');
+avalancheWarning('I-70 between Copper Mountain and Vail');
+wildfireWarning('US-36 near Boulder');
